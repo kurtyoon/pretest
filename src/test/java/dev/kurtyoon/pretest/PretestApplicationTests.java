76c0a048b31a301fb7841ac8f334ba60cb46f30d
kurtyoon <akakertcobain@naver.com>
2025-02-25 00:33:13 +0900

    init: Init Project

---
+++ src/test/java/dev/kurtyoon/pretest/PretestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kurtyoon.pretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PretestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
