1ae9de17bec2a7c26c9338b63debcf6beae2cf83
kurtyoon <akakertcobain@naver.com>
2025-02-27 00:28:19 +0900

    feat: Time Zone Setting when Setup

--- src/main/java/dev/kurtyoon/pretest/PretestApplication.java
+++ src/main/java/dev/kurtyoon/pretest/PretestApplication.java
@@ -1,10 +1,18 @@
 package dev.kurtyoon.pretest;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PretestApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PretestApplication.class, args);
