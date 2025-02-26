752712c985fbd879e622e5305f846e6d4a8ed2bd
kurtyoon <akakertcobain@naver.com>
2025-02-26 22:15:28 +0900

    refactor: Request Command Dto Validation

--- src/main/java/dev/kurtyoon/pretest/application/dto/request/OrderCommand.java
+++ src/main/java/dev/kurtyoon/pretest/application/dto/request/OrderCommand.java
@@ -1,23 +1,25 @@
 package dev.kurtyoon.pretest.application.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
 
 import java.util.List;
 
 public record OrderCommand(
 
         @JsonProperty("customer_name")
-        @NotNull
+        @NotBlank(message = "고객 이름은 필수입니다.")
+        @Pattern(regexp = "^[가-힣a-zA-Z0-9\\s]*$", message = "고객 일므에 특수문자를 사용할 수 없습니다.")
         String customerName,
 
         @JsonProperty("customer_address")
-        @NotNull
+        @NotBlank(message = "배송 주소는 필수입니다.")
         String customerAddress,
 
         @JsonProperty("items")
-        @NotEmpty
+        @NotEmpty(message = "주문 항목은 최소 1개 이상이어야 합니다.")
+        @Valid
         List<OrderItemCommand> items
 ) {
 }
--- src/main/java/dev/kurtyoon/pretest/application/dto/request/OrderItemCommand.java
+++ src/main/java/dev/kurtyoon/pretest/application/dto/request/OrderItemCommand.java
@@ -2,20 +2,21 @@ package dev.kurtyoon.pretest.application.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record OrderItemCommand(
 
         @JsonProperty("product_id")
-        @NotNull
+        @NotNull(message = "상품 ID는 필수입니다.")
         Long productId,
 
         @JsonProperty("product_name")
-        @NotNull
+        @NotBlank(message = "상품명은 필수입니다.")
         String productName,
 
         @JsonProperty("quantity")
-        @Min(1)
+        @Min(value = 1, message = "주문 수량은 최소 1개 이상이어야 합니다.")
         int quantity
 ) {
 }
